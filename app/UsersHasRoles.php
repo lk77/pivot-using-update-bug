48303501e2dc23e8743f5955c75cd93aa2142a34
Mathias Eudeline <m.eudeline@crown.fr>
2019-08-02 08:56:18 +0200

    Pulling https://github.com/mpyw/framework.git @ dev-fix-updating-custom-pivot-updated-at

--- app/UsersHasRoles.php
+++ app/UsersHasRoles.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class UsersHasRoles extends Pivot
 {
+    public $timestamps = true;
+
     /**
      * The attributes that are mass assignable.
      *
